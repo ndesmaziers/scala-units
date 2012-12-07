250f7bd55ca95fe7f42d941e7189637735692e7b
Nicolas <ndesmaziers@gmail.com>
2012-12-07 15:18:57 +0100

    go

--- units/src/main/scala/com/blaasoft/units/CommonUnits.scala
+++ units/src/main/scala/com/blaasoft/units/CommonUnits.scala
@@ -1,5 +1,5 @@
 package com.blaasoft.units
 
 object CommonUnits {
-
+	val kilometer = new PrefixedUnit(Prefix.kilo, AtomicUnit.meter)
 }
--- units/src/main/scala/com/blaasoft/units/Test.scala
+++ units/src/main/scala/com/blaasoft/units/Test.scala
@@ -3,6 +3,8 @@ package com.blaasoft.units
 object Test {
 
   def main(args: Array[String]): scala.Unit = {
+    println(CommonUnits.kilometer)
+    
   }
 
 }
